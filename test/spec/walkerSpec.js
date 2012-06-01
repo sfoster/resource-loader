1c709452ed9e9848ef83563bfc6d22602111767d
sfoster <sam@sam-i-am.com>
2012-06-01 11:22:01 +0100

    Type checks on the parsed basic json example

--- test/spec/walkerSpec.js
+++ test/spec/walkerSpec.js
@@ -1,5 +1,7 @@
-define(['walker', 'text!test/data/basicDataTypes.json'], function(walker, basicDataTypesString){
-
+define(['walker', 'type', 'text!test/data/basicDataTypes.json'], function(walker, type, basicDataTypesString){
+  
+  var is = type.is;
+  
   describe("Walk", function() {
 
     it("should instantiate ok", function() {
@@ -20,6 +22,23 @@ define(['walker', 'text!test/data/basicDataTypes.json'], function(walker, basicD
       expect(err).toBeFalsy();
       expect(doc).toBeTruthy();
       expect(typeof doc).toBe('object');
+    });
+
+    it("should parse basic data types", function(){
+      var doc = walker.parse(basicDataTypesString);
+      
+      expect( type(doc) ).toBe('object');
+      expect( type(doc.emptyarray) ).toBe('array');
+      expect( type(doc.emptyobject) ).toBe('object');
+      expect( type(doc.emptystring) ).toBe('string');
+      expect( type(doc.emptynumber) ).toBe('number');
+      expect( type(doc.anull) ).toBe('null');
+      expect( type(doc.arrayofarrays[0]) ).toBe('array');
+      expect( type(doc.arrayofobjects[0]) ).toBe('object');
+      expect( type(doc.arrayofprimitives[0]) ).toBe('string');
+      expect( type(doc.arrayofprimitives[1]) ).toBe('number');
+      expect( type(doc.arrayofprimitives[1]) ).toBe('number');
+      
     });
 
   });
