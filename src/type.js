edaa52e2e98f861588427ef39fd4e0af29c7cd73
sfoster <sam@sam-i-am.com>
2012-06-01 11:21:40 +0100

    type module providing type(thing) for string name, and is(thing) for boolean type check

---
+++ src/type.js
@@ -0,0 +1,38 @@
+define(function(undef){
+  var empty = {};
+  var is = {
+    'undefined': function(thing){
+      return thing === undef;
+    },
+    'number': function(thing){
+      return 'number' == typeof thing || thing instanceof Number;
+    },
+    'string': function(thing){
+      return 'string' == typeof thing || thing instanceof String;
+    },
+    'array': function(thing){
+      return is.object(thing) && thing instanceof Array;
+    },
+    'function': function(thing){
+      return 'function' == typeof thing;
+    },
+    'null': function(thing){
+      return thing === null;
+    },
+    'object': function(thing){
+      return 'object' == typeof thing;
+    }
+  };
+  
+  function type(thing){
+    for(var name in is){
+      if(name in empty) continue;
+      if(is[name](thing)) return name;
+    }
+    return 'unknown';
+  }
+  
+  type.is = is; 
+  
+  return type;
+});
